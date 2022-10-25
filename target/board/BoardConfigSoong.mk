d12ad9244b6c629e47960f9e2ece75cfa4a54f07
Khalvat-M <mmba313@gmail.com>
2022-10-25 06:41:39 +0330

    Revert "config: Remove TARGET_DISABLE_POSTRENDER_CLEANUP"
    [ImSpiDy: adapt to aospa]
    
    This reverts commit fa5bc45a95fae4cbd68b3d19265f5f1e799792e2.
    
    Change-Id: Ifbca1afd1518c082313c6dbdaacb163e1cfcc706

--- target/board/BoardConfigSoong.mk
+++ target/board/BoardConfigSoong.mk
@@ -18,6 +18,7 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 SOONG_CONFIG_NAMESPACES += aospaGlobalVars
 SOONG_CONFIG_aospaGlobalVars += \
     aapt_version_code \
+    disable_postrender_cleanup \
     needs_camera_boottime \
     powershare_node \
     target_camera_package_name \
@@ -43,6 +44,7 @@ TARGET_INIT_VENDOR_LIB ?= vendor_init
 TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
+SOONG_CONFIG_aospaGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_aospaGlobalVars_aapt_version_code := $(shell date -u +%Y%m%d)
 SOONG_CONFIG_aospaGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_aospaGlobalVars_powershare_node := $(TARGET_POWERSHARE_NODE)
